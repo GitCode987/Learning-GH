f55ad31deb67cc97eddcd911940e48edac4da091
Ken McGlothlin <ken@jetware.com>
2022-07-08 12:44:05 -0700

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ken McGlothlin on 7/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
